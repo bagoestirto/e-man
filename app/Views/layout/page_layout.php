378ae1f8285581ba24b8a007d47c0b3ba0dfe6ab
bagoestirto <bagoestirto@gmail.com>
2022-07-05 15:19:36 +0700

    menambahkan template & memisahkannya per part

---
+++ app/Views/layout/page_layout.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="keywords" content="wrappixel, admin dashboard, html css dashboard, web dashboard, bootstrap 5 admin, bootstrap 5, css3 dashboard, bootstrap 5 dashboard, Matrix lite admin bootstrap 5 dashboard, frontend, responsive bootstrap 5 admin template, Matrix admin lite design, Matrix admin lite dashboard bootstrap 5 dashboard template" />
+    <meta name="description" content="Matrix Admin Lite Free Version is powerful and clean admin dashboard template, inpired from Bootstrap Framework" />
+    <meta name="robots" content="noindex,nofollow" />
+    <title>Matrix Admin Lite Free Versions Template by WrapPixel</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href=" <?= base_url('/assets/images/smk.png'); ?>" />
+    <!-- Custom CSS -->
+    <link href="<?= base_url('assets/libs/flot/css/float-chart.css'); ?>" rel="stylesheet" />
+    <!-- Custom CSS -->
+    <link href="<?= base_url('dist/css/style.min.css'); ?>" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full" data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+        <!-- ============================================================== -->
+        <!-- Topbar header - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+        <?= $this->include('layout/navbar'); ?>
+        <!-- ============================================================== -->
+        <!-- End Topbar header -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <?= $this->include('layout/sidebar'); ?>
+        <!-- ============================================================== -->
+        <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <div class="page-breadcrumb">
+                <div class="row">
+                    <div class="col-12 d-flex no-block align-items-center">
+                        <h4 class="page-title">Dashboard</h4>
+                        <div class="ms-auto text-end">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">
+                                        Library
+                                    </li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <?= $this->renderSection('content'); ?>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- footer -->
+            <!-- ============================================================== -->
+            <footer class="footer text-center">
+                All Rights Reserved by Matrix-admin. Designed and Developed by
+                <a href="https://www.wrappixel.com">WrapPixel</a>.
+            </footer>
+            <!-- ============================================================== -->
+            <!-- End footer -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="<?= base_url('assets/libs/jquery/dist/jquery.min.js'); ?>"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="<?= base_url('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/extra-libs/sparkline/sparkline.js'); ?>"></script>
+    <!--Wave Effects -->
+    <script src="<?= base_url('dist/js/waves.js'); ?>"></script>
+    <!--Menu sidebar -->
+    <script src="<?= base_url('dist/js/sidebarmenu.js'); ?>"></script>
+    <!--Custom JavaScript -->
+    <script src="<?= base_url('dist/js/custom.min.js'); ?>"></script>
+    <!--This page JavaScript -->
+    <!-- <script src="../dist/js/pages/dashboards/dashboard1.js"></script> -->
+    <!-- Charts js Files -->
+    <script src="<?= base_url('assets/libs/flot/excanvas.js'); ?>"></script>
+    <script src="<?= base_url('assets/libs/flot/jquery.flot.js'); ?>"></script>
+    <script src="<?= base_url('assets/libs/flot/jquery.flot.pie.js'); ?>"></script>
+    <script src="<?= base_url('assets/libs/flot/jquery.flot.time.js'); ?>"></script>
+    <script src="<?= base_url('assets/libs/flot/jquery.flot.stack.js'); ?>"></script>
+    <script src="<?= base_url('assets/libs/flot/jquery.flot.crosshair.js'); ?>"></script>
+    <script src="<?= base_url('assets/libs/flot.tooltip/js/jquery.flot.tooltip.min.js'); ?>"></script>
+    <script src="<?= base_url('dist/js/pages/chart/chart-page-init.js'); ?>"></script>
+</body>
+
+</html>
