a0a7538f15fab761eb128c570c530d55e24b9e55
bagoestirto <bagoestirto@gmail.com>
2022-07-21 00:31:27 +0700

    db kari neh

--- ##/eman.sql
+++ ##/eman.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : LOKALAN
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 100422
+ Source Server Version : 100424
  Source Host           : localhost:3306
  Source Schema         : eman
 
  Target Server Type    : MySQL
- Target Server Version : 100422
+ Target Server Version : 100424
  File Encoding         : 65001
 
- Date: 19/07/2022 15:13:48
+ Date: 21/07/2022 00:30:43
 */
 
 SET NAMES utf8mb4;
@@ -41,13 +41,13 @@ CREATE TABLE `tb_barang`  (
 -- ----------------------------
 -- Records of tb_barang
 -- ----------------------------
-INSERT INTO `tb_barang` VALUES (1, 'PRS/BOS/2021', 'PRS-BOS-2021', 'Printer Scan', 2, 'ASET TETAP', 'EPSON', 'L3110', '001-BOS', '2021-06-11', 'Unit', 'BAIK', 7300000);
+INSERT INTO `tb_barang` VALUES (1, 'PRS/BOS/2021', 'PRS-BOS-2021', 'Printer Scan', 1, 'ASET TETAP', 'EPSON', 'L3110', '001-BOS', '2021-06-11', 'Unit', 'BAIK', 7300000);
 INSERT INTO `tb_barang` VALUES (2, 'KAD/BOS/2021', 'KAD-BOS-2021', 'Kipas Angin DInding', 2, 'ASET TETAP', 'MASPION', 'Maspion', '001-BOS', '2021-06-11', 'Unit', 'BAIK', 1700000);
 INSERT INTO `tb_barang` VALUES (3, 'SRA/BOS/2021', 'SRA-BOS-2021', 'Scanner Portable', 8, 'ASET TETAP', 'CANON', 'Epson', '001-BOS', '2021-06-11', 'Unit', 'BAIK', 4235000);
-INSERT INTO `tb_barang` VALUES (4, 'CPUKI/BOS/2021', 'CPUKI-BOS-2021', 'CPU Rakitan', 4, 'ASET TETAP', '-', 'Core i5', '001-BOS', '2021-06-11', 'Unit', 'BAIK', 15000000);
+INSERT INTO `tb_barang` VALUES (4, 'CPUKI/BOS/2021', 'CPUKI-BOS-2021', 'CPU Rakitan', 3, 'ASET TETAP', '-', 'Core i5', '001-BOS', '2021-06-11', 'Unit', 'BAIK', 15000000);
 INSERT INTO `tb_barang` VALUES (5, 'KAD/BOS/2021', 'KAD-BOS-2021', 'Kipas Angin Dinding', 2, 'ASET TETAP', 'MASPION', 'PW-501W', '001-BOS', '2021-11-26', 'Unit', 'BAIK', 1700000);
 INSERT INTO `tb_barang` VALUES (6, 'CPURE/BOS/2021', 'CPURE-BOS-2021', 'CPU Core i5', 3, 'ASET TETAP', '-', '-', '001-BOS', '2021-11-26', 'Unit', 'BAIK', 3750000);
-INSERT INTO `tb_barang` VALUES (7, 'MNR/BOS/2021', 'MNR-BOS-2021', 'Monitor 16\"', 3, 'ASET TETAP', '-', 'LG', '001-BOS', '2021-11-26', 'Unit', 'BAIK', 6375000);
+INSERT INTO `tb_barang` VALUES (7, 'MNR/BOS/2021', 'MNR-BOS-2021', 'Monitor 16\"', 1, 'ASET TETAP', '-', 'LG', '001-BOS', '2021-11-26', 'Unit', 'BAIK', 6375000);
 INSERT INTO `tb_barang` VALUES (8, 'LTP/BOS/2021', 'LTP-BOS-2021', 'Laptop', 2, 'ASET TETAP', 'ASUS', 'K513EA-OLED321', '001-BOS', '2021-11-26', 'Unit', 'BAIK', 9850000);
 INSERT INTO `tb_barang` VALUES (9, 'MUI/BOS/2021', 'MUI-BOS-2021', 'Meja Kursi Siswa', 57, 'ASET TETAP', '-', '-', '001-BOS', '2021-11-26', 'Set', 'BAIK', 66000000);
 INSERT INTO `tb_barang` VALUES (10, 'MUG/BOS/2021', 'MUG-BOS-2021', 'Meja Kursi Guru', 6, 'ASET TETAP', '-', '-', '001-BOS', '2021-11-26', 'Set', 'BAIK', 1650000);
@@ -83,6 +83,13 @@ CREATE TABLE `tb_detail_peminjaman`  (
   `kode_barang` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `jumlah` int(11) NOT NULL
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_detail_peminjaman
+-- ----------------------------
+INSERT INTO `tb_detail_peminjaman` VALUES ('1', '1', 1);
+INSERT INTO `tb_detail_peminjaman` VALUES ('2', '4', 1);
+INSERT INTO `tb_detail_peminjaman` VALUES ('2', '7', 2);
 
 -- ----------------------------
 -- Table structure for tb_detail_perawatan
@@ -139,7 +146,7 @@ CREATE TABLE `tb_pegawai`  (
 -- Records of tb_pegawai
 -- ----------------------------
 INSERT INTO `tb_pegawai` VALUES (' 197609292008012017', 'Ariah Sejati, S.Pd', 'ariah-sejati-spd', 'SUMBERANYAR - PAITON', '081323665878');
-INSERT INTO `tb_pegawai` VALUES ('111', '13213200', '13213200', '54654654600', '54654654600');
+INSERT INTO `tb_pegawai` VALUES ('111', 'Pegawai', '13213200', '54654654600', '54654654600');
 INSERT INTO `tb_pegawai` VALUES ('123456', 'apa aja lah', 'apa-aja-lah', 'aaaaaaaaa', '4444444');
 INSERT INTO `tb_pegawai` VALUES ('196810112007012019', 'Dra. Anik Nursukma', 'dra-anik-nursukma', 'TRIWUNGAN - KOTAANYAR', '085269336554');
 INSERT INTO `tb_pegawai` VALUES ('197109042005012009', 'Fatratin Azizah, SE', 'fatratin-azizah-se', 'KRAKSAAN - KRAKSAAN', '083117228745');
@@ -156,12 +163,20 @@ INSERT INTO `tb_pegawai` VALUES ('198505122010012020', 'Iva Dwi Meilydiawati, S.
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_peminjaman`;
 CREATE TABLE `tb_peminjaman`  (
-  `kode_pinjam` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `kode_pinjam` int(15) NOT NULL,
   `username` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `tgl_pinjam` datetime(0) NOT NULL,
   `tgl_jatuh_tempo` datetime(0) NOT NULL,
-  `kode_pegawai` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+  `kode_pegawai` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `status` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`kode_pinjam`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_peminjaman
+-- ----------------------------
+INSERT INTO `tb_peminjaman` VALUES (1, 'bagoestirto', '2022-07-20 21:17:35', '2022-07-27 21:17:41', '111', 'Keluar');
+INSERT INTO `tb_peminjaman` VALUES (2, 'bagoestirto', '2022-07-20 00:00:00', '2022-07-27 00:00:00', '197211242006041007', 'Keluar');
 
 -- ----------------------------
 -- Table structure for tb_penghapusan
@@ -201,12 +216,17 @@ CREATE TABLE `tb_sumberdana`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_user`;
 CREATE TABLE `tb_user`  (
-  `id_user` int(10) NOT NULL,
+  `id_user` int(10) NOT NULL AUTO_INCREMENT,
   `username` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `nama_user` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `jabatan` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id_user`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_user
+-- ----------------------------
+INSERT INTO `tb_user` VALUES (1, 'bagoestirto', 'Bagus Tirto', 'Admin', '$2y$10$XQSi0HCBgBWZFQ2FZzaWQOIKJ./SqquuTkjJlvxpNO2D69XDhHeIi');
 
 SET FOREIGN_KEY_CHECKS = 1;
